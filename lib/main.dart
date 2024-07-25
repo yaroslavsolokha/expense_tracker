e7dbab47eb9c7dc866a67d040d2c8c9c6d80bc3e
Jaroslav Solocha <jaroslav.solocha@gmail.com>
2024-07-25 14:29:58 +0200

    lock on portrait up orientation (commented)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
@@ -11,6 +12,9 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  //WidgetsFlutterBinding.ensureInitialized();
+  //SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+  //  (fn) => {
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
@@ -45,5 +49,7 @@ void main() {
       // themeMode: ThemeMode.system, // it is by default
       home: const Expenses(),
     ),
+    // ),
+    //},
   );
 }
