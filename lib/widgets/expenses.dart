a3401cdb339872d15623e66ce4280558f615cadd
Jaroslav Solocha <jaroslav.solocha@gmail.com>
2024-07-25 15:16:44 +0200

    implementing responsines

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -37,6 +37,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(
@@ -77,6 +78,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some...'),
     );
@@ -98,12 +101,17 @@ class _ExpensesState extends State<Expenses> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _reqisteredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _reqisteredExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(children: [
+              Expanded(child: Chart(expenses: _reqisteredExpenses)),
+              Expanded(child: mainContent),
+            ]),
     );
   }
 }
